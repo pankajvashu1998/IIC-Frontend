b3f00271eb839186e65f2f9d50d7597415dc9160
pankajvashu1998 <pk85764798@gmail.com>
2024-10-20 08:40:31 -0700

    Student zone UI Design 1

--- src/Component/Student Zone/View Result/View_Result.jsx
+++ src/Component/Student Zone/View Result/View_Result.jsx
@@ -7,7 +7,7 @@ function View_Result() {
               <div className='p-4 w-full bg-gray-200 md:flex justify-between'>
                   <div><h1 className='text-2xl font-bold text-gray-600 underline pb-3'>Results</h1></div>
                   <div>
-                    <form className='flex'>
+                    <form className='flex items-center'>
                     <input type="text" placeholder='Enter Reg. No.' className='border px-2 py-2 mx-1 rounded w-full'/>
                     <input type="text" placeholder='Enter Name' className='border px-2 py-2 mx-1 rounded w-full'/>
                     <button className='bg-yellow-600 rounded text-white font-semibold px-2 py-2 mx-1'>Search</button>
